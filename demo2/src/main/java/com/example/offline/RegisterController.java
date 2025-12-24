25fe67597cd560de346fe2eb7cbaefab7f4216c1
Jimit <140408993+Jimit6921@users.noreply.github.com>
2025-12-24 14:50:38 +0530

    Update RegisterController.java

--- demo2/src/main/java/com/example/offline/RegisterController.java
+++ demo2/src/main/java/com/example/offline/RegisterController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class RegisterController {
 
+    @GetMapping("/")
+    public String home() {
+        return "Backend is running";
+    }
+
     @PostMapping("/register")
     public String register(@RequestBody User user) {
         System.out.println("Name: " + user.getName());
